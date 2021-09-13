4b6aba1d7f16a7a73045dce1a195d357b19553be
EgijaLocmele <locmele.egija@gmail.com>
2021-09-13 11:14:19 +0300

    Code shortening

--- src/features/productDetails/ProductDetails.js
+++ src/features/productDetails/ProductDetails.js
@@ -1,15 +1,11 @@
 import { useParams } from 'react-router-dom'
-import PropTypes from 'prop-types'
 import ProductDetailsItem from './ProductDetailsItem'
 
-const ProductDetails = (props) => {
-  const { products } = props
+const ProductDetails = ({ products }) => {
   const { itemId } = useParams()
   const selectBook = products?.find((book) => book.id === itemId)
 
   return <ProductDetailsItem products={selectBook} />
 }
-ProductDetails.propTypes = {
-  products: PropTypes.array
-}
+
 export default ProductDetails
